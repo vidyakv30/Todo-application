588c5ba7e14c8df103a96769c2d74cbdcfcf14f8
Vidya Kalappurayil Viswanathan <vk427@njit.edu>
2017-12-17 23:13:38 -0500

    Added form styling for change password
    Signed-off-by: Vidya Kalappurayil Viswanathan <vk427@njit.edu>

--- pages/change_password.php
+++ pages/change_password.php
@@ -17,33 +17,46 @@
 
 <body>
 
-
 <div class="container">
-    <div class="row">
-        <div class="col-xs-12">
-            <h3 class="text-center">Reset Password</h3>
-            <hr/>
-            <form action ="index.php?page=accounts&action=changePassword" method= "POST">
-                <div class="form-group">
-                    <label>Old Password :</label>
-                    <input type="password" class="form-control" name="oldpassword"/>
-                </div>
-                <div class="form-group">
-                    <label>New Password :</label>
-                    <input type="password" class="form-control" name="newpassword"/>
-                </div>
-                <div class="form-group">
-                    <label>Confirm Password :</label>
-                    <input type="password" class="form-control" name="confirmpassword"/>
-                </div>
-                <button class="btn btn-primary btn-sm center-block" type="submit">
-                    Submit
-                </button>
-            </form>
+    <div id="changepswdbox" style="margin-top:50px"
+         class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+        <div class="panel panel-info">
+            <div class="panel-heading">
+                <div class="panel-title">Change Password</div>
+                <div style="float:right; font-size: 85%; position: relative; top:-10px"></div>
+            </div>
+            <div class="panel-body">
+                <form id="changePswdForm" action="index.php?page=accounts&action=changePassword"
+                      method="post" class="form-horizontal" role="form">
+
+                    <div class="form-group">
+                        <label for="oldpassword" class="col-md-3 control-label">Old Password</label>
+                        <div class="col-md-9">
+                            <input type="password" class="form-control" name="oldpassword"><br>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="newpassword" class="col-md-3 control-label">New Password</label>
+                        <div class="col-md-9">
+                            <input type="password" class="form-control" name="newpassword"><br>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="confirmpassword" class="col-md-3 control-label">Confirm Password</label>
+                        <div class="col-md-9">
+                            <input type="password" class="form-control" name="confirmpassword"><br>
+                        </div>
+                    </div>
+                    <button class="btn btn-primary btn-sm center-block" type="submit">
+                        Submit
+                    </button>
+                </form>
+            </div>
         </div>
     </div>
 </div>
-
 
 <script src="js/scripts.js"></script>
 </body>
